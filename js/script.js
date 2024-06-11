166497f92825893a766cce507fb3f7567aeaa88d
VMolo4ke <85548094+VMolo4ke@users.noreply.github.com>
2024-06-11 22:24:35 +0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+        console.log(href)
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 80;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
